33df9e93145898b72a0698ebfaa0d22e9706e403
jithin <jithinpjoji@gmail.com>
2026-04-26 10:55:51 +0530

    updated the Chat with us button to take you to the Support Hub selection page instead of opening the chat directly

--- client/src/pages/myorders/OrderDetail.jsx
+++ client/src/pages/myorders/OrderDetail.jsx
@@ -323,7 +323,7 @@ export default function OrderDetail() {
                 </div>
                 {isDelivered && (
                   <div className="od-chat-section">
-                    <button className="od-chat-btn" onClick={() => navigate('/support/chat', { state: { order } })}>
+                    <button className="od-chat-btn" onClick={() => navigate('/support/order-help', { state: { order } })}>
                       <svg width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2">
                         <path d="M21 15a2 2 0 0 1-2 2H7l-4 4V5a2 2 0 0 1 2-2h14a2 2 0 0 1 2 2z"></path>
                       </svg>
